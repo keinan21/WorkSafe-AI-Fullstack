a7f523e772a7ac9ca9c76028a57051e1f1404d42
keinan21 <yusufgamesintara@gmail.com>
2026-05-28 14:25:59 +0700

    feat : mengubah firebase agar pake .env saja

--- backend/src/config/firebase.js
+++ backend/src/config/firebase.js
@@ -1,6 +1,7 @@
-// src/config/firebase.js
 const admin = require('firebase-admin');
-const serviceAccount = require('./firebase-service-account.json');
+
+// Baca dari env, lalu parse teks string-nya menjadi objek JSON asli
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
